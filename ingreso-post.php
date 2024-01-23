a284f0fbbdaed1849f55a1a3e5e44feccc0cb69f
Afrecho <147094113+Afrecho@users.noreply.github.com>
2024-01-23 08:46:08 -0500

    Add files via upload
    subiendo foto

--- ingreso-post.php
+++ ingreso-post.php
@@ -1,38 +1,40 @@
-
 <?php
 
 $servername = "localhost";
 $username = "id20925312_factor";
 $password = "U*4rL@b9&zP2";
-$dbname = "id20925312_forovilla"; 
-
+$dbname = "id20925312_forovilla";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
-
 $titulo = $_POST['titulo'];
-$fecha = date('Y-m-d'); 
+$fecha = date('Y-m-d');
 $user_id = $_POST['user_id'];
 $contenido = $_POST['contenido'];
-$imagen_url = $_POST['imagen_url'];
 $video_url = $_POST['video_url'];
 $tags = $_POST['tags'];
 
+$imagen_dir = "imgUpload/";
+$imagen_nombre = $_FILES['imagen']['name'];
+$imagen_temp = $_FILES['imagen']['tmp_name'];
+$imagen_ruta = $imagen_dir . $imagen_nombre;
 
-$sql = "INSERT INTO posts (titulo, fecha, user_id, contenido, imagen_url, video_url, tags)
-        VALUES ('$titulo', '$fecha', $user_id, '$contenido', '$imagen_url', '$video_url', '$tags')";
+if (move_uploaded_file($imagen_temp, $imagen_ruta)) {
+    $sql = "INSERT INTO posts (titulo, fecha, user_id, contenido, imagen_url, video_url, tags)
+            VALUES ('$titulo', '$fecha', $user_id, '$contenido', '$imagen_ruta', '$video_url', '$tags')";
 
-if ($conn->query($sql) === TRUE) {
-    echo "Datos ingresados con éxito.";
+    if ($conn->query($sql) === TRUE) {
+        echo "Datos ingresados con éxito.";
+    } else {
+        echo "Error en la inserción: " . $conn->error;
+    }
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    echo "Error al subir la imagen.";
 }
 
-// chau mrd
 $conn->close();
 ?>
